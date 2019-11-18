12678eea9e0d59ca212bbc3eb1f05c3662c1494c
NajdanM <57903840+NajdanM@users.noreply.github.com>
2019-11-18 21:17:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
 import pygame as pg
 import random as rng
+
 pg.init()
 
 background=(137,207,240) 
-(width, height) = (600,500)
-screen = pg.display.set_mode((width, height))
-screen.fill(background)
-pg.display.set_caption('Flappy bird')
-pg.display.flip()
-running = True
-pipes = pg.image.load('pipe.png') 
-bird = pg.image.load('bird.png')
-ground = pg.image.load('ground.png')
+(width, height)=(600,500)
 e=rng.randint(1,150)
 x=400
 y=170
-vel= 8 
-jmp= 15
-g= -15  
+vel=8 
+jmp=15
+g=-15
 jmp_pressed=5
 jump_finished=0
 jump_fall=5
+
+screen=pg.display.set_mode((width, height))
+screen.fill(background)
+pg.display.set_caption('Flappy bird')
+pg.display.flip()
+pipes=pg.image.load('pipe.png') 
+bird=pg.image.load('bird.png')
+ground=pg.image.load('ground.png')
+running=True
+
 while running: 
     pg.time.delay(50)
+
+    for event in pg.event.get():
+        if event.type==pg.QUIT:
+            running=False 
+
     keys = pg.key.get_pressed()
-    if y > 0:
+    if y>0:
         if keys[pg.K_SPACE] and jump_finished==0:
             jmp_pressed=1   
             jmp=15 
-            jump_finished=1
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False   
-    screen.blit(ground, (0,470))       
-    screen.blit(pipes, (x,310+e))
-    screen.blit(pipes, (x,-410+e))
-    screen.blit(bird, (50,y))
+            jump_finished=1  
     x-=vel
     if jmp_pressed<5:
         jmp_pressed+=1
@@ -45,7 +46,6 @@ while running:
     else:
         if jump_fall<5:
             jump_fall+=1
-            g=-7
             y-=g
             g-=2
         else:
@@ -53,7 +53,12 @@ while running:
         if jmp_pressed==5:
             jump_finished=0
     if y>440:
-        running = False         
+        running = False      
+
+    screen.blit(ground, (0,470))       
+    screen.blit(pipes, (x,310+e))
+    screen.blit(pipes, (x,-410+e))
+    screen.blit(bird, (50,y))    
     pg.display.update()  
     screen.fill(background)
           
